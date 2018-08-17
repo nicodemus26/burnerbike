88276dad420ac38440085dbf88de7713eeae6605
Nicodemus Paradiso <nicodemus26@gmail.com>
2018-08-17 01:21:42 +0000

    Two buttons, cycling and brightness

--- button.py
+++ button.py
@@ -2,25 +2,30 @@
 import time
 import sys
 import RPi.GPIO as GPIO
-BUTTON_GPIO=23
+BRIGHTNESS_BUTTON=23
+CYCLE_BUTTON=24
+
+brightness=256
+cycle=0
+
+def brightness_pressed(val):
+    global brightness
+    brightness = brightness * 2
+    if brightness > 256:
+        brightness = 1
+    print("brightness to %d with %s" % (brightness,val))
+
+def cycle_pressed(val):
+    global cycle
+    cycle = cycle + 1
+    if cycle > 10:
+        cycle = 1
+    print("cycled to %d with %s" % (cycle,val))
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_GPIO,GPIO.IN)
+GPIO.setup([BRIGHTNESS_BUTTON, CYCLE_BUTTON],GPIO.IN)
+GPIO.add_event_detect(BRIGHTNESS_BUTTON, GPIO.RISING, callback=brightness_pressed, bouncetime=50)
+GPIO.add_event_detect(CYCLE_BUTTON, GPIO.RISING, callback=cycle_pressed, bouncetime=50)
 
-#initialise a previous input variable to 0 (assume button not pressed last)
-i = 0
-prev_input = 0
 while True:
-  #take a reading
-  input = GPIO.input(BUTTON_GPIO)
-  #if the last reading was low and this one high, print
-  if ((not prev_input) and input):
-    time.sleep(0.02) # make sure this is not a transient click, poll again right after
-    input = GPIO.input(BUTTON_GPIO)
-    if ((not prev_input) and input):
-      print("Button pressed %d" % (i,))
-      i = i + 1
-  #update previous input
-  prev_input = input
-  #slight pause to debounce
-  time.sleep(0.05)
+    time.sleep(1)
